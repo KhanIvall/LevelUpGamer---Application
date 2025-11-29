4d53a1a49450869eef31a35ebc3d5516c6d0ec55
KhanIvall <ar.arayao@duocuc.cl>
2025-11-29 15:52:44 -0300

    fix en el login

--- app/src/main/java/com/example/level_up_gamer/ui/components/RegistroContent.kt
+++ app/src/main/java/com/example/level_up_gamer/ui/components/RegistroContent.kt
@@ -1,2 +1,10 @@
 package com.example.level_up_gamer.ui.components
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.example.level_up_gamer.viewmodel.LoginViewModel
+
+@Composable
+fun RegistroContent(navController: NavController) {
+    //val viewModel: LoginViewModel = viewModel()
+}
--- app/src/main/java/com/example/level_up_gamer/ui/screen/RegistroScreen.kt
+++ app/src/main/java/com/example/level_up_gamer/ui/screen/RegistroScreen.kt
@@ -1,17 +1,11 @@
 package com.example.level_up_gamer.ui.screen
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.level_up_gamer.ui.components.RegistroContent
 import com.example.level_up_gamer.viewmodel.LoginViewModel
 
 @Composable
@@ -19,5 +13,9 @@ fun RegistroScreen(navController: NavController) {
     //val viewModel: LoginViewModel = viewModel()
 
     //val errorApi by viewModel.error.observeAsState()
+
+    RegistroContent(
+        navController = navController
+    )
 
 }
